b84e7b34933850570f10db9f280844e05abd07a8
may h <mehrabhoque@gmail.com>
2021-07-11 21:08:14 -0400

    Expand parser tests to be table-driven and handle errors

--- query/liquid/parser_test.go
+++ query/liquid/parser_test.go
@@ -8,44 +8,66 @@ import (
 )
 
 func TestParser(t *testing.T) {
-	parser := liquid.NewParser("find foo = 4.2 bar >7 car=\"hello\" | sort bar")
-	stages, err := parser.Parse()
-	assert.NoError(t, err)
-	assert.Equal(t,
-		[]liquid.Stage{
-			&liquid.Find{
-				Checks: []*liquid.Check{
-					{
-						Field: "foo",
-						Value: &liquid.Const{
-							Kind:        liquid.ConstKindNumber,
-							Stringified: "4.2",
+	type testCase struct {
+		query  string
+		errMsg string
+		stages []liquid.Stage
+	}
+
+	tcs := []testCase{
+		{
+			query: "find foo = 4.2 bar >7 car=\"hello\" | sort bar",
+			stages: []liquid.Stage{
+				&liquid.Find{
+					Checks: []*liquid.Check{
+						{
+							Field: "foo",
+							Value: &liquid.Const{
+								Kind:        liquid.ConstKindNumber,
+								Stringified: "4.2",
+							},
+							Op: liquid.BinaryOpEquals,
 						},
-						Op: liquid.BinaryOpEquals,
-					},
-					{
-						Field: "bar",
-						Value: &liquid.Const{
-							Kind:        liquid.ConstKindNumber,
-							Stringified: "7",
+						{
+							Field: "bar",
+							Value: &liquid.Const{
+								Kind:        liquid.ConstKindNumber,
+								Stringified: "7",
+							},
+							Op: liquid.BinaryOpGeq,
 						},
-						Op: liquid.BinaryOpGeq,
-					},
-					{
-						Field: "car",
-						Value: &liquid.Const{
-							Kind:        liquid.ConstKindString,
-							Stringified: "hello",
+						{
+							Field: "car",
+							Value: &liquid.Const{
+								Kind:        liquid.ConstKindString,
+								Stringified: "hello",
+							},
+							Op: liquid.BinaryOpEquals,
 						},
-						Op: liquid.BinaryOpEquals,
 					},
 				},
-			},
-			&liquid.Sort{
-				Descending: false,
-				Field:      "bar",
+				&liquid.Sort{
+					Descending: false,
+					Field:      "bar",
+				},
 			},
 		},
-		stages,
-	)
+		{
+			query:  "find f",
+			errMsg: "failed to parse: failed to parse check: failed to parse op: expected a binary operator, but reached end of query",
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.query, func(t *testing.T) {
+			parser := liquid.NewParser(tc.query)
+			stages, err := parser.Parse()
+			if tc.errMsg == "" {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.stages, stages)
+			} else {
+				assert.EqualError(t, err, tc.errMsg)
+			}
+		})
+	}
 }
