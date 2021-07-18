565830728efa5be9aad31294a6ee53b05b3655c4
may h <mehrabhoque@gmail.com>
2021-07-18 16:33:48 -0400

    Add doc comments to types in ast.go

--- query/breeze/ast.go
+++ query/breeze/ast.go
@@ -5,14 +5,19 @@ import (
 	"strconv"
 )
 
+// ConstKind enumerates the kinds of constants in breeze.
 type ConstKind string
 
 const (
+	// ConstKindString represents a string constant.
 	ConstKindString ConstKind = "string"
+	// ConstKindNumber represents a number constant.
 	ConstKindNumber ConstKind = "number"
-	ConstKindBool   ConstKind = "bool"
+	// ConstKindBool represents a bool constant.
+	ConstKindBool ConstKind = "bool"
 )
 
+// Const is a constant.
 type Const struct {
 	Kind        ConstKind
 	Stringified string
@@ -22,6 +27,8 @@ func (c *Const) String() string {
 	return c.Stringified
 }
 
+// Interface returns this constant, casted from its stringified version into the
+// proper Go type.
 func (c *Const) Interface() interface{} {
 	switch c.Kind {
 	case ConstKindString:
@@ -36,14 +43,19 @@ func (c *Const) Interface() interface{} {
 	}
 }
 
+// BinaryOp enumerates the kinds of binary operations in breeze.
 type BinaryOp string
 
 const (
-	BinaryOpEquals   BinaryOp = "="
-	BinaryOpGeq               = ">"
-	BinaryOpContains          = "contains"
+	// BinaryOpEquals is the equality operation.
+	BinaryOpEquals BinaryOp = "="
+	// BinaryOpGeq is the greater-than-or-equal operation.
+	BinaryOpGeq = ">"
+	// BinaryOpContains is the contains operation.
+	BinaryOpContains = "contains"
 )
 
+// Check is a filter condition for find.
 type Check struct {
 	Field string
 	Value *Const
@@ -54,23 +66,28 @@ func (c *Check) String() string {
 	return fmt.Sprintf("%s %s %v", c.Field, c.Op, c.Value)
 }
 
+// Find is a stage that applies a series of filters to the data.
 type Find struct {
 	Checks []*Check
 }
 
+// Name implements the Stage interface.
 func (f *Find) Name() string {
 	return "find"
 }
 
+// Sort is a stage that sorts the data.
 type Sort struct {
 	Descending bool
 	Field      string
 }
 
+// Name implements the Stage interface.
 func (s *Sort) Name() string {
 	return "sort"
 }
 
+// Stage is a single unit of processing to apply to the data.
 type Stage interface {
 	Name() string
 }
