41d173676c128ebedb5c375cc6e93fb99b662f27
may h <mehrabhoque@gmail.com>
2021-08-31 19:05:07 -0400

    Move the aggregator implementations to their own file

---
+++ query/breeze/execution/aggregator.go
@@ -0,0 +1,158 @@
+package execution
+
+type aggregator interface {
+	ingest(value interface{})
+	aggregate() interface{}
+}
+
+type sum struct {
+	numberTotal float64
+	numNumbers  int
+
+	// sum can handle both floats and bools (for bools, we are taking an OR of the
+	// entire set). We track the counts of each so we know which one to give back
+	// when Aggregate() is called.
+	boolTotal bool
+	numBools  int
+}
+
+// TODO: This can overflow, and so can some other agg functions.
+func (s *sum) ingest(value interface{}) {
+	if floatValue, ok := convertPotentialNumber(value); ok {
+		s.numberTotal += floatValue
+	} else if boolValue, ok := convertPotentialBool(value); ok {
+		s.boolTotal = s.boolTotal || boolValue
+	}
+	// Otherwise, do nothing.
+}
+
+func (s *sum) aggregate() interface{} {
+	if s.numNumbers >= s.numBools {
+		return s.numberTotal
+	}
+	return s.boolTotal
+}
+
+type avg struct {
+	total     sum
+	numValues int
+}
+
+func (a *avg) ingest(value interface{}) {
+	var ingestibleValue float64 = 0
+	switch ta := value.(type) {
+	case bool:
+		ingestibleValue = 0
+		if ta {
+			ingestibleValue = 1
+		}
+	case float64:
+		ingestibleValue = ta
+	default:
+		ingestibleValue = 0
+	}
+	a.total.ingest(ingestibleValue)
+	a.numValues++
+}
+
+func (a *avg) aggregate() interface{} {
+	totalSum := a.total.aggregate()
+	switch tsum := totalSum.(type) {
+	case float64:
+		if a.numValues == 0 {
+			return 0
+		}
+		return tsum / float64(a.numValues)
+	default:
+		panic("TODO")
+	}
+}
+
+type count struct {
+	numValues uint
+}
+
+func (c *count) ingest(_ interface{}) {
+	c.numValues++
+}
+
+func (c *count) aggregate() interface{} {
+	return c.numValues
+}
+
+// min and max can be implemented with the same general type, but since the
+// amount of code duplication reduction will be small, we choose to duplicate
+// it with simpler code.
+type min struct {
+	minimumVal interface{}
+}
+
+func (m *min) ingest(v interface{}) {
+	if m.minimumVal == nil {
+		m.minimumVal = v
+		return
+	}
+
+	switch Compare(v, m.minimumVal) {
+	case Lesser:
+		m.minimumVal = v
+	}
+}
+
+func (m *min) aggregate() interface{} {
+	return m.minimumVal
+}
+
+type max struct {
+	maximumVal interface{}
+}
+
+func (m *max) ingest(v interface{}) {
+	switch Compare(v, m.maximumVal) {
+	case Greater:
+		m.maximumVal = v
+	}
+}
+
+func (m *max) aggregate() interface{} {
+	return m.maximumVal
+}
+
+type mode struct {
+	valueCounts *table
+}
+
+func (m *mode) ingest(v interface{}) {
+	if m.valueCounts == nil {
+		m.valueCounts = newTable()
+	}
+
+	untypedCnt, ok := m.valueCounts.GetOK(v)
+	if ok {
+		cnt := untypedCnt.(uint)
+		m.valueCounts.Set(v, cnt+1)
+	} else {
+		m.valueCounts.Set(v, uint(1))
+	}
+}
+
+func (m *mode) aggregate() interface{} {
+	if m.valueCounts == nil {
+		m.valueCounts = newTable()
+	}
+
+	keys := m.valueCounts.Keys()
+
+	maxCount := -1
+	var maxCountKey interface{} = nil
+	for _, key := range keys {
+		untypedCnt := m.valueCounts.Get(key)
+		cnt := int(untypedCnt.(uint))
+		if cnt > maxCount {
+			maxCount = cnt
+			maxCountKey = key
+		}
+	}
+
+	return maxCountKey
+}
